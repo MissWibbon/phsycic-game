6a8241d4095fc52859cec3aa5d8d0a8950fef7d5
MissWibbon <wuebben.michelle@yahoo.com>
2019-06-29 15:44:15 -0500

    added logic to wins and losses

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,17 +1,58 @@
 var wins = 0;
 var losses = 0;
 var guesses = 0;
-var guess;
-var guessesLeft = 0;
+var guessesLeft = 9;
 var guessArray = ["a", "s", "d", "f", "g", "h", "j", "k", "l", ";"];
-var guessLimit = 10;
+var letterGuesses = [];
+var computerKey = guessArray[Math.floor(Math.random()*guessArray.length)];
 
-document.addEventListener("keydown", function guess() {
-    const userKey = event.key;
-    document.getElementById("#guess").innerHTML = userKey;
-    const computerKey = guessArray[Math.floor(Math.random()*guessArray.length)];
-    console.log(userKey);
-    if(userKey === computerKey) {
-        wins = wins + 1;
-    }
-});
+
+
+    document.onkeyup = function(event) {
+        var userGuess = event.key;
+        letterGuesses.push(userGuess);
+        document.getElementById("guess").innerHTML = userGuess;
+        document.getElementById("computerGuess").innerHTML = computerKey;
+        //console.log(userKey);
+        document.getElementById("guesses").innerHTML = letterGuesses;            
+
+        
+        
+
+        console.log(guessesLeft);
+
+        if(userGuess === computerKey) {
+            wins++;            
+            computerKey = guessArray[Math.floor(Math.random()*guessArray.length)];
+            document.getElementById("wins").innerHTML = wins;
+            letterGuesses = [];
+            guessesLeft = 9;
+            document.getElementById("guessesLeft").innerHTML = guessesLeft;
+
+            //alert("Correct!");
+
+        }
+        else if (guessesLeft === 0){
+            computerKey = guessArray[Math.floor(Math.random()*guessArray.length)];
+            losses = losses + 1;        
+            document.getElementById("losses").innerHTML = losses;
+
+            //alert("you lose!");
+            letterGuesses = [];
+            guessesLeft = 9;
+            document.getElementById("guessesLeft").innerHTML = guessesLeft;
+
+        }
+        else {
+            guesses = guesses + 1;
+            guessesLeft = guessesLeft - 1;        
+            document.getElementById("guessesLeft").innerHTML = guessesLeft;
+
+        }
+
+    };
+    function guessLimit() {
+        if(guesses >= guessesLeft ) {
+            alert("you lost!");
+        }
+    };
