64d90e84f574c4e2a278236914f81b2bf60bf2ce
MissWibbon <wuebben.michelle@yahoo.com>
2019-06-29 14:00:49 -0500

    added IDs and got userKey

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,4 +1,17 @@
 var wins = 0;
 var losses = 0;
 var guesses = 0;
+var guess;
 var guessesLeft = 0;
+var guessArray = ["a", "s", "d", "f", "g", "h", "j", "k", "l", ";"];
+var guessLimit = 10;
+
+document.addEventListener("keydown", function guess() {
+    const userKey = event.key;
+    document.getElementById("#guess").innerHTML = userKey;
+    const computerKey = guessArray[Math.floor(Math.random()*guessArray.length)];
+    console.log(userKey);
+    if(userKey === computerKey) {
+        wins = wins + 1;
+    }
+});
